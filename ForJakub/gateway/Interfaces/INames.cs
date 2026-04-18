084565f1d0ed4bbb709c39d59aa78147836b56b9
Nieman <lukasz_golojuch@o2.pl>
2026-04-18 14:11:13 +0200

    Add names system to work less on reflections

---
+++ ForJakub/gateway/Interfaces/INames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ForJakub.gateway.Interfaces
+{
+    internal interface INames<T> where T : Enum
+    {
+        public string GetFriendlyName(T enumName);
+
+        public string GetFieldName(T enumName);
+    }
+}
