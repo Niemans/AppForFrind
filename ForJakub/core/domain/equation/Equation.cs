66050a0f28a32856c5c660e383073d71f44b9bf9
Nieman <lukasz_golojuch@o2.pl>
2026-06-28 16:31:53 +0200

    Create Token and Lexer for equations

---
+++ ForJakub/core/domain/equation/Equation.cs
@@ -0,0 +1,52 @@
+﻿using System.Buffers;
+using System.Text.RegularExpressions;
+
+namespace ForJakub.core.domain.equation;
+
+internal record Equation
+{
+    public readonly List<Token> tokens = [];
+
+    public Equation(string input)
+    {
+        Lexer(input);
+    }
+
+    private static string RemoveWhiteSpace(string input)
+    {
+        return string.Concat(input.Where(c => !char.IsWhiteSpace(c)));
+    }
+
+    private void Lexer(string input)
+    {
+        var equation = string.Concat([..input.Where(c => !char.IsWhiteSpace(c))]).ToLower();
+
+        List<string> operators =
+        [
+            ..
+            Token.Operators
+                .Where(equation.Contains)
+                .Select(c => c)
+        ];
+
+        var dict = operators.ToDictionary(op => op, _ => new List<int>());
+        var matches = Regex.Matches(equation, string.Join("|", operators.Select(Regex.Escape)));
+        foreach (Match match in matches)
+        {
+            dict[match.Value].Add(match.Index);
+        }
+
+        List<(int start, int end)> operatorsPlaces = [..dict.SelectMany(t => t.Value, (t, op) => (op, t.Key.Length))];
+        HashSet<int> uniqueIndexes = [0];
+        uniqueIndexes.UnionWith(operatorsPlaces.Select(res => res.start));
+        uniqueIndexes.UnionWith(operatorsPlaces.Select(res => res.start + res.end));
+        uniqueIndexes = [.. uniqueIndexes.OrderBy(i => i)];
+
+        var uIndexesList = uniqueIndexes.ToList();
+        for (var i = 0; i < uIndexesList.Count - 1; i++)
+        {
+            tokens.Add(new Token(equation[uIndexesList[i]..uIndexesList[i + 1]]));
+        }
+        tokens.Add(new Token(equation[uIndexesList[^1]..]));
+    }
+}
---
+++ ForJakub/core/domain/equation/Token.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using static System.Double;
+
+namespace ForJakub.core.domain.equation;
+
+public class Token
+{
+    public readonly Func<double[], double> operation;
+    private readonly double _value;
+    public static readonly HashSet<string> Operators =
+    [
+        "sin",
+        "log",
+        "log10",
+        "^",
+        "*",
+        "/",
+        "%",
+        "+",
+        "-",
+        "-x"
+    ];
+
+    public Token(string tokenString)
+    {
+        operation = GetOperator(TryParse(tokenString, out var parsed) ? "number" : tokenString);
+        _value = parsed;
+    }
+    
+    
+    private Token(double value)
+    {
+        operation = GetOperator(value.ToString(CultureInfo.InvariantCulture));
+        _value = value;
+    }
+
+    private Func<double[], double> GetOperator(string input)
+    {
+        return input switch
+        {
+            "number"    => _ => _value,
+            "sin"       => a => Math.Sin(a[0]),
+            "log"       => a => Math.Log(a[0], a[1]),
+            "log10"     => a => Math.Log10(a[0]),
+            "^"         => a => Math.Pow(a[0], a[1]),
+            "*"         => a => a[0] * a[1],
+            "/"         => a => a[0] / a[1],
+            "%"         => a => a[0] % a[1],
+            "+"         => a => a[0] + a[1],
+            "-"         => a => a[0] - a[1],
+            "-x"        => a => -a[0],
+            _           => _ => 0.0
+        };
+    }
+
+    public static implicit operator Token(double value) => new(value);
+    public static implicit operator double(Token token) => token._value;
+}
